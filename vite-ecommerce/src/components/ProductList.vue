950e7247dd5e9af78c940e31b732f23b21883ffe
Thapelo <thapeloletsapa@gmail.com>
2024-08-02 11:43:25 +0200

    "Added ProductList.vue component with template, script, and import for ProductCard"

---
+++ vite-ecommerce/src/components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <ProductCard v-for="product in products" :key="product.id" :product="product" @view-details="viewDetails" />
+    </div>
+  </template>
+  
+  <script>
+  import ProductCard from './ProductCard.vue';
+  
+  export default {
+    components: {
+      ProductCard
+    },
+    props: {
+      products: Array
+    },
+    methods: {
+      viewDetails(product) {
+        this.$router.push({ name: 'Product', params: { id: product.id } });
+      }
+    }
+  };
+  </script>
+  
