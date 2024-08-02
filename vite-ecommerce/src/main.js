ffca5f365f02b9a76276b573d8e97e63cf61d399
Thapelo <thapeloletsapa@gmail.com>
2024-08-02 11:45:53 +0200

    "Refactor App.vue and main.js to use Vue router and store"

--- vite-ecommerce/src/main.js
+++ vite-ecommerce/src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import './style.css'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
+
