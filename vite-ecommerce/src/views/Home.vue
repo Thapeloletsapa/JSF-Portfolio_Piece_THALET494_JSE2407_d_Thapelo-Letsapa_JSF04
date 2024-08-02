3bd378c012847e4ff10800f00a824d8521adce8c
Thapelo <thapeloletsapa@gmail.com>
2024-08-02 11:46:17 +0200

    "Added Home.vue with ProductList component and Vuex integration"

---
+++ vite-ecommerce/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1 class="text-3xl font-bold mb-4">Products</h1>
+      <ProductList :products="products" />
+    </div>
+  </template>
+  
+  <script>
+  import ProductList from '../components/ProductList.vue';
+  import { mapState } from 'vuex';
+  
+  export default {
+    components: {
+      ProductList
+    },
+    computed: {
+      ...mapState(['products'])
+    },
+    created() {
+      this.$store.dispatch('fetchProducts');
+    }
+  };
+  </script>
+  
