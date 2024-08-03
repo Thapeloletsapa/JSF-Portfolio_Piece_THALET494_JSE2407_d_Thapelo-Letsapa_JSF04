9f1dbc0c31ddf480101d942555dc145c8dfb230b
Thapelo <thapeloletsapa@gmail.com>
2024-08-03 18:04:27 +0200

    "Updated ProductCard.vue to add 'bg-contain' class to img element, and modified grid layout in ProductList.vue"

--- vite-ecommerce/src/components/ProductList.vue
+++ vite-ecommerce/src/components/ProductList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div class="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       <ProductCard v-for="product in products" :key="product.id" :product="product" @view-details="viewDetails" />
     </div>
   </template>
