fc5b53443242beddc82906906c3b700548df8c15
Thapelo <thapeloletsapa@gmail.com>
2024-08-06 13:11:15 +0200

    Added an img element to the Header component.

--- vite-ecommerce/src/components/Header.vue
+++ vite-ecommerce/src/components/Header.vue
@@ -2,6 +2,7 @@
     <header class="bg-gray-800 p-4 text-white">
       <div class="container mx-auto flex justify-between items-center">
         <router-link to="/" class="text-xl font-bold">Home</router-link>
+        <img src="../assets/Shopify_E-commerce_Logo_Web_Design_PNG_-_Free_Download-removebg-preview.png" className="h-11" alt="Flowbite Logo" />
         <router-link to="/cart" class="text-xl font-bold">Cart</router-link>
       </div>
     </header>
